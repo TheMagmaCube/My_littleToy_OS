d3ed9b2d2ad2f56617c3c3db8f618f2753b34898
TheMagmaCube <mailroxu@gmail.com>
2024-09-18 17:51:07 +0200

    Update to OS 0.2 add: 1. Exceptions for user inputs. 2. Fix subtract 3. Fix clean memory for new user operations

--- src/com/main/java/MyLittleToyOS/os/calculator.java
+++ src/com/main/java/MyLittleToyOS/os/calculator.java
@@ -11,42 +11,57 @@ public class calculator {
     private ArrayList<String> numbers = new ArrayList<>();
     private ArrayList<Double> double_numbers;
     private ArrayList<String> sings = new ArrayList<>();
+    private boolean exception = false;
+    private String choice;
+    private boolean number;
 
-
-    public void Menu(boolean number) {
+    public void Menu() {
         while (true) {
             number = true;
-            String choice = scanner.nextLine();
+            choice = scanner.nextLine();
             ingredients_of_action = choice.split(" ");
             for (int i = 0; i < ingredients_of_action.length; i++) {
-                if(number){
+                if (number) {
                     numbers.add(ingredients_of_action[i]);
                     number = false;
-                }else if(!number){
+                } else if (!number) {
                     sings.add(ingredients_of_action[i]);
                     number = true;
                 }
-
             }
             calculate();
         }
     }
     private void calculate(){
         convert();
-        while(!sings.isEmpty()){
-        times(0);
-        division(0);
-        add(0);
-        subtract(0);
+        if(exception != true) {
+            while (!sings.isEmpty()) {
+                times(0);
+                division(0);
+                subtract(0);
+                add(0);
+            }
+            System.out.println(double_numbers.get(0));
+            clear();
+        }else{
+            clear();
         }
-        System.out.print(" = " + double_numbers.get(0));
+    }
+    private void clear(){
         double_numbers.clear();
-
+        numbers.clear();
+        sings.clear();
     }
     private void convert(){
-        double_numbers = new ArrayList<Double>(numbers.size());
-        for(int i = 0; i < numbers.size(); i++){
-            double_numbers.add(i, Double.parseDouble(numbers.get(i)));
+        try {
+            exception = false;
+            double_numbers = new ArrayList<Double>(numbers.size());
+            for (int i = 0; i < numbers.size(); i++) {
+                double_numbers.add(i, Double.parseDouble(numbers.get(i)));
+            }
+        } catch (NumberFormatException e){
+            exception = true;
+            System.err.println("You must to type number.");
         }
     }
     private double times(double result){
